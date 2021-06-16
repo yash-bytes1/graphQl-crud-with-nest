9715ef4294c9b30372fbfca82a3463c68cb36751
yash-bytes1 <yash.sanja@bytestechnolab.in>
2021-06-16 12:04:10 +0530

    add redis and grpc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AppService } from './app.service';
-import { MathService } from './math.service';
+import { Controller, Post, Body, OnModuleInit } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { MathService } from "./math.service";
+import { ClientGrpc, Client, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { IGrpcService } from "./grpc.interface";
+import { join } from "path";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,private readonly mathService:MathService) {}
+  //implements OnModuleInit
 
-  @Post('add')
-  async accumulate(@Body('data') data: number[]) {
+  // @Client({
+  //   transport: Transport.GRPC,
+  //   options: {
+  //     package: "app",
+  //     protoPath: join(__dirname, "../src/app.proto")
+  //   }
+  // })
+  // private client: ClientGrpc;
+  // private grpcService: IGrpcService;
+
+  constructor(private readonly appService: AppService,
+              private readonly mathService: MathService
+  ) {  }
+
+  // onModuleInit() {
+  //   this.grpcService = this.client.getService<IGrpcService>('AppController')
+  // }
+
+  @Post("add")
+  async accumulate(@Body("data") data: number[]) {
+    // return this.mathService.accumulate({ data });
     return this.mathService.accumulate(data);
   }
 }
--- src/math.service.ts
+++ src/math.service.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { join } from "path" ;
 
 @Injectable()
 export class MathService {
-  private client: ClientProxy
+  private client: ClientProxy;
 
-  constructor(){
+  constructor() {
+    // this.client = ClientProxyFactory.create({
+    //   transport: Transport.TCP,
+    //   options:{
+    //     host:'127.0.0.1',
+    //     port: 8877
+    //   }
+    // })
     this.client = ClientProxyFactory.create({
-      transport: Transport.TCP,
-      options:{
-        host:'127.0.0.1',
-        port: 8877
-      }
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      },
     })
   }
 
-  public accumulate(data: number[]) {             
-   return this.client.send<number, number[]>('add', data)
+  public accumulate(data: number[]) {
+    return this.client.send<number, number[]>("add", data);
   }
 }
 
